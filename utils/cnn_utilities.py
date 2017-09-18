bdbed2dff7961e22a562c7656f1633e7e8993adb
Burak Himmetoglu <corellia@Buraks-MacBook-Pro.local>
2017-09-18 00:54:12 -0700

    Corrected train/test discrepancy and updated reported accuracies

--- utils/cnn_utilities.py
+++ utils/cnn_utilities.py
@@ -1,12 +1,14 @@
 ## EEG classification utilities for 1d CNN
 # Author: B. Himmetoglu
-# 8/15/2017
+# Original    : 8/15/2017
+# Last Update : 9/14/2017
 
 from random import shuffle
 import numpy as np
 import pickle
 import os
 import glob
+import random
 from scipy.io import loadmat
 from sklearn.model_selection import train_test_split, StratifiedKFold
 
@@ -19,24 +21,123 @@ def list_data(data_path):
 
 	return clips_interictal, clips_preictal, clips_test
 
-## Split into train/validation (stratified)
-def split_data(subjects, validation_size = 0.2, random_state = 123):
+## Split up each clip in pieces and return tensor
+def split_segment(fil, new_seq_len = 1200):
+	""" fil         : File containing the segment to be split
+		new_seq_len : New segment length
+		#	
+		Given X.shape = (n_channels, T), the returned tensor will have shape
+		[T/new_seq_len, seq_len, n_channels] 
+
+	"""
+
+	# Read from file
+	clip = loadmat(fil)
+	segment_name = [el for el in list(clip.keys()) if "segment" in el][0] # Get segment name
+	input_segment = clip[segment_name][0][0][0] # Get electrode data
+	sampling_freq = np.squeeze(clip[segment_name][0][0][1]) # Sampling frequency
 	
+	# Get number of channels
+	n_channels = clip[segment_name][0][0][0].shape[0]
+	
+	# Split by sampling_freq
+	n_ch, L_seg = input_segment.shape
+	assert n_ch == n_channels, "Wrong number of channels!"
+
+	# Total duration
+	T_seg = int(np.ceil(L_seg / sampling_freq))
+
+	# Save in another array and pad by zeros for processing
+	X = np.zeros((n_channels, T_seg*sampling_freq))
+	X[:,:input_segment.shape[1]] = input_segment
+
+	# Target
+	t = 1 if "preictal" in segment_name else 0
+
+	# Initiate list of arrays and targets
+	X_ = []
+	Y_ = []
+	indices = np.arange(X.shape[1]).reshape(-1, new_seq_len)
+	for f in range(len(indices)):
+		# Extract pieces
+		temp = X[:,indices[f]]
+
+		# Reshape into tensor
+		X_.append(temp.reshape(1, temp.shape[1], temp.shape[0]))
+		Y_.append(t)
+
+	# Return
+	return np.concatenate(X_, axis = 0), np.array(Y_, dtype = np.int)
+
+
+## Generate samples from inputs
+def generate_samples(seg_files, T_seg, new_seq_len = 1200, batch_size = 400):
+	""" seg_files   : A given set of files containing 0/1 segments
+		T_seg       : Exact length of one sequence
+		new_seq_len : The new sequence length
+		batch_size  : The size of the batch
+	"""
+
+	# Get 1/0 segments within a given list of files
+	preictal_segs = [f for f in seg_files if "preictal" in f]
+	interictal_segs = [f for f in seg_files if "interictal" in f]
+
+	# Determine 0/1 ratio
+	ratio_ = int(np.ceil(len(interictal_segs) / len(preictal_segs)))
+	n_inter = ratio_ * len(preictal_segs) # Number of interictal segments for one preictal
+
+	# Construct balanced segs
+	balanced_segs = []
+	for ii,pr in enumerate(preictal_segs):
+		balanced_segs.append(pr)
+		balanced_segs += interictal_segs[ii*n_inter:(ii+1)*n_inter]
+
+	# Check size (remove later)
+	assert len(balanced_segs) == len(seg_files), "Wrong number of segments in the balanced list!"
+
+	# Batch ratio: (Batch size) / (# of blocks for each segment)
+	len_data = T_seg // new_seq_len
+	batch_ratio = batch_size // len_data
+
+	assert batch_ratio > 0, "Batch size too small!"
+
+	# Loop over segments and yield batches
+	for ii in range(0,len(balanced_segs), batch_ratio):
+		current_segs = balanced_segs[ii:(ii+batch_ratio)]
+		X_ = []
+		Y_ = []
+		for jj in range(batch_ratio):
+			x,y = split_segment(current_segs[jj],new_seq_len)
+			X_.append(x)
+			Y_.append(y)
+
+		# Yield
+		yield np.concatenate(X_, axis = 0), np.array(Y_, dtype = np.int)
+
+
+## TO BE COMPLETED: A Function to create test samples
+def generate_test_samples():
+	pass
+
+def split_train_validation(subject, validation_size = 0.2, random_state = 123):
+	""" Split into training and test sets """
+
+	# The location of data
+	folder_name = os.path.join("../data/", subject)
+
+	# Now divide into train/validation sets 
+	interictal, preictal, test = list_data(folder_name)
+
+	# Lists for training files and labels (0/1 : interictal/preictal)
 	train_files = []
 	labels = []
-	# Loop over subjects
-	for subj in subjects:
-		path_name = "../data/" + subj
 
-		# Get data
-		interictal, preictal, test = list_data(path_name)
-
-		# Splitting into batches
-		files = interictal + preictal
-		for el in files:
-			t = 1 if "preictal" in el else 0
-			labels.append(t)
-			train_files.append(el)
+	# Splitting into batches
+	files = interictal + preictal
+	for el in files:
+		t = 1 if "preictal" in el else 0
+		labels.append(t)
+		train_files.append(el)
 
 	# Split into train/validation
 	fil_tr, fil_vld, lab_tr, lab_vld = train_test_split(train_files, labels, stratify=labels, 
@@ -44,95 +145,48 @@ def split_data(subjects, validation_size = 0.2, random_state = 123):
 
 	return fil_tr, fil_vld, lab_tr, lab_vld
 
-## Batch generator (stratified) Note: final batch will have length batch_size*n_blocks
-def get_batches(fil, lab, batch_size = 60, n_blk = 6, n_ch = 16, s_freq = 600, blk_s = 60):
 
-	# Number of batches
-	n_batches = len(fil) // batch_size
-
-	# Make sure that each batch has similar distribution
-	skf = StratifiedKFold(n_splits = n_batches, random_state = 1234)
-
-	for _, ii in skf.split(fil, lab):
-		batch_segments = [fil[ifil] for ifil in ii]
-		data_batch, label_batch = cnn_build_input(batch_segments, 
-			n_blocks = n_blk, n_ch = n_ch, s_freq = s_freq, block_s = blk_s)
-		
-		yield data_batch, label_batch
-
-
-## Collect all the segments to build a tesnsor input for LSTM. Uses the function lstm_sequence
-def cnn_build_input(clips, n_blocks = 6, n_ch = 16, s_freq = 600, block_s = 60):
-	""" Collect all the data and build sequences for LSTM
-		clips              : List of clips
-		s_freq             : Sampling frequency
-		block_s            : Size of the block in seconds (default = 60)
+## Generate balanced samples from inputs
+def generate_samples_balanced(seg_files, T_seg, new_seq_len = 1200, batch_size = 400):
+	""" seg_files   : A given set of files containing 0/1 segments
+		new_seq_len : The new sequence length
+		batch_size  : 
 	"""
 
-	# Number of clips
-	n_clips = len(clips)
+	# Get 1/0 segments within a give list of files
+	preictal_segs = [f for f in seg_files if "preictal" in f]
+	interictal_segs = [f for f in seg_files if "interictal" in f]
 
-	# Initiate
-	block_len = s_freq * block_s   # Length of each block 
-	X_full = np.zeros((n_clips*n_blocks, block_len, n_ch))
-	Y_full = np.zeros((n_clips*n_blocks,1))
+	# Determine 0/1 ratio
+	ratio_ = int(np.ceil(len(interictal_segs) / len(preictal_segs)))
+	n_inter = ratio_ * len(preictal_segs) # Number of interictal segments for one preictal
 
-	# Loop over all clips and store data
-	iclip = 0
-	for fil in clips:
-		clip = loadmat(fil)
-		segment_name = [el for el in list(clip.keys()) if "segment" in el][0] # Get segment name
-		input_segment = clip[segment_name][0][0][0] # Get electrode data
-		sampling_freq = np.squeeze(clip[segment_name][0][0][1]) # Sampling frequency
-		assert sampling_freq == s_freq, "Wrong sampling frequency!"
+	# Get 1/0 segments within a give list of files
+	preictal_segs = [f for f in seg_files if "preictal" in f]
+	interictal_segs = [f for f in seg_files if "interictal" in f]
 
-		# Get number of channels
-		n_channels = clip[segment_name][0][0][0].shape[0]
-		assert n_channels == n_ch, "Wrong number of channels!"
+	# Determine 0/1 ratio
+	ratio_ = int(np.ceil(len(interictal_segs) / len(preictal_segs)))
+	n_inter = ratio_ * len(preictal_segs) # Number of interictal segments for one preictal
 
-		# Get target
-		target = 1 if "preictal" in segment_name else 0
+	# Randomly shuffle each time the generator is called
+	random.shuffle(interictal_segs) 
 
-		# Get tensor
-		X, Y = cnn_tensor(input_segment, target, n_ch, n_blocks, block_len)
+	# Loop over balaned (preictal,interictal) pieces and generate batches
+	for ii,pr in enumerate(preictal_segs):
+		X_pr, Y_pr = split_segment(pr, new_seq_len)
+		#n_batch = len(X_pr) // batch_size
+		#X_pr,Y_pr = X_pr[:n_batch*batch_size], Y_pr[:n_batch*batch_size]
 
-		# Save in full tensor
-		X_full[iclip*n_blocks:(iclip+1)*n_blocks,:,:] = X
-		Y_full[iclip*n_blocks:(iclip+1)*n_blocks] = Y[:,None]
+		#assert batch_size <= len(X_pr), "Batch size must be at most {:d}".format(len(X_pr))
 
-		iclip += 1
+		for interic in interictal_segs[ii*ratio_:(ii+1)*ratio_]:
+			X_itr, Y_itr = split_segment(interic, new_seq_len)
+			n_batch = len(X_itr) // batch_size
+			X_itr,Y_itr = X_itr[:n_batch*batch_size], Y_itr[:n_batch*batch_size]
 
-	# Return
-	return X_full, Y_full
-
-## Construct seuqnces from one segment
-def cnn_tensor(input_segment, target, n_channels, n_blocks, block_len):
-	""" Function for generating blocks of input tensors 
-		input_segment : The EEG segment
-		target        : 1/0 (preictal/interictial); None for test
-		n_channels    : Number of channels
-		n_blocks      : Number of blocks
-		block_len     : Length of each block (= sampling_freq * block_s )
-	"""
-
-	# Dimensions
-	n_channels, T_segment = input_segment.shape
-
-	# Check block dimensions
-	n_blk = (T_segment-1) // block_len # Number of blocks
-	assert n_blk == n_blocks, "Block number mistmatch!, {:d} vs {:d}".format(n_blk, n_blocks)
-
-	# Split into blocks
-	input_segment = input_segment[:,:n_blocks*block_len]
-	X = input_segment.reshape(-1, n_blocks, block_len)
-	X = np.transpose(X, [1,2,0])
-	# final shape: n_blocks, block_len, n_channels
-
-	# Fill in the target
-	if (target == 1):
-		Y = np.ones(n_blocks)
-	elif(target == 0):
-		Y = np.zeros(n_blocks)
-
-	# Return
-	return X, Y
+			# Yield batches
+			for b in range(0,len(X_itr), batch_size):
+				X_batch = np.vstack((X_pr, X_itr[b:b+batch_size]))
+				Y_batch = np.hstack((Y_pr, Y_itr[b:b+batch_size]))
+				yield X_batch, Y_batch
--- utils/cnn_utils.py
+++ utils/cnn_utils.py
@@ -1,16 +1,133 @@
 ## EEG classification utilities for 1d CNN
 # Author: B. Himmetoglu
-# 8/14/2017
+# Original    : 8/15/2017
+# Last Update : 9/1/2017
 
+from random import shuffle
 import numpy as np
 import pickle
 import os
+import glob
 from scipy.io import loadmat
-from utils.get_prepare_data_full import get_data
+from sklearn.model_selection import train_test_split, StratifiedKFold
+
+## Get list of data
+def list_data(data_path):
+
+	clips_interictal = glob.glob(os.path.join(data_path, "*interictal*"))
+	clips_preictal = glob.glob(os.path.join(data_path, "*preictal*"))
+	clips_test = glob.glob(os.path.join(data_path, "*test*"))
+
+	return clips_interictal, clips_preictal, clips_test
+
+## Split into train/validation (stratified)
+def split_data(subjects, validation_size = 0.2, random_state = 123):
+	
+	train_files = []
+	labels = []
+	# Loop over subjects
+	for subj in subjects:
+		path_name = "../data/" + subj
+
+		# Get data
+		interictal, preictal, test = list_data(path_name)
+
+		# Splitting into batches
+		files = interictal + preictal
+		for el in files:
+			t = 1 if "preictal" in el else 0
+			labels.append(t)
+			train_files.append(el)
+
+	# Split into train/validation
+	fil_tr, fil_vld, lab_tr, lab_vld = train_test_split(train_files, labels, stratify=labels, 
+		test_size = validation_size, random_state = random_state)
+
+	return fil_tr, fil_vld, lab_tr, lab_vld
+
+
+## Batch generator: 
+def get_batches(fil, lab, batch_size = 60, n_blk = 6, n_ch = 16, s_freq = 600, blk_s = 60):
+
+	# For stratified splits
+	n_preictal = sum(lab) # Number of preictal segments
+	n_splits = len(fil) // n_preictal 
+	skf = StratifiedKFold(n_splits = n_splits, random_state = 1234)
+
+	for _, ii in skf.split(fil, lab):
+		batch_segments = [fil[ifil] for ifil in ii]
+		data_batch, label_batch = cnn_build_input(batch_segments, 
+			n_blocks = n_blk, n_ch = n_ch, s_freq = s_freq, block_s = blk_s)
+
+		n_batch = len(data_batch) // batch_size
+		data_batch, label_batch = data_batch[:n_batch*batch_size], label_batch[:n_batch*batch_size]
+
+
+
+## Batch generator (old) Note: final batch will have length batch_size*n_blocks
+def get_batches_(fil, lab, batch_size = 60, n_blk = 6, n_ch = 16, s_freq = 600, blk_s = 60):
+
+	# Number of batches
+	n_batches = len(fil) // batch_size
+
+	# Make sure that each batch has similar distribution
+	skf = StratifiedKFold(n_splits = n_batches, random_state = 1234)
+
+	for _, ii in skf.split(fil, lab):
+		batch_segments = [fil[ifil] for ifil in ii]
+		data_batch, label_batch = cnn_build_input(batch_segments, 
+			n_blocks = n_blk, n_ch = n_ch, s_freq = s_freq, block_s = blk_s)
+		
+		yield data_batch, label_batch
+
+
+## Collect all the segments to build a tesnsor input for CNN
+def cnn_build_input(clips, n_blocks = 6, n_ch = 16, s_freq = 600, block_s = 60):
+	""" Collect all the data and build sequences 
+		clips              : List of clips
+		s_freq             : Sampling frequency
+		block_s            : Size of the block in seconds (default = 60)
+	"""
+
+	# Number of clips
+	n_clips = len(clips)
+
+	# Initiate
+	block_len = s_freq * block_s   # Length of each block 
+	X_full = np.zeros((n_clips*n_blocks, block_len, n_ch))
+	Y_full = np.zeros((n_clips*n_blocks,1))
+
+	# Loop over all clips and store data
+	iclip = 0
+	for fil in clips:
+		clip = loadmat(fil)
+		segment_name = [el for el in list(clip.keys()) if "segment" in el][0] # Get segment name
+		input_segment = clip[segment_name][0][0][0] # Get electrode data
+		sampling_freq = np.squeeze(clip[segment_name][0][0][1]) # Sampling frequency
+		assert sampling_freq == s_freq, "Wrong sampling frequency!"
+
+		# Get number of channels
+		n_channels = clip[segment_name][0][0][0].shape[0]
+		assert n_channels == n_ch, "Wrong number of channels!"
+
+		# Get target
+		target = 1 if "preictal" in segment_name else 0
+
+		# Get tensor
+		X, Y = cnn_tensor(input_segment, target, n_ch, n_blocks, block_len)
+
+		# Save in full tensor
+		X_full[iclip*n_blocks:(iclip+1)*n_blocks,:,:] = X
+		Y_full[iclip*n_blocks:(iclip+1)*n_blocks] = Y[:,None]
+
+		iclip += 1
+
+	# Return
+	return X_full, Y_full
 
 ## Construct seuqnces from one segment
 def cnn_tensor(input_segment, target, n_channels, n_blocks, block_len):
-	""" Function for generating blocks of LSTM input tensors 
+	""" Function for generating blocks of input tensors 
 		input_segment : The EEG segment
 		target        : 1/0 (preictal/interictial); None for test
 		n_channels    : Number of channels
@@ -36,156 +153,6 @@ def cnn_tensor(input_segment, target, n_channels, n_blocks, block_len):
 		Y = np.ones(n_blocks)
 	elif(target == 0):
 		Y = np.zeros(n_blocks)
-	else:
-		Y = None
 
 	# Return
 	return X, Y
-
-## Collect all the segments to build a tesnsor input for LSTM. Uses the function lstm_sequence
-def cnn_build_input(clips, target, n_blocks = 6, n_ch = 16, s_freq = 600, block_s = 60):
-	""" Collect all the data and build sequences for LSTM
-		clips              : List of clips
-		target             : 1/0 (preictal/interictial); None for test set
-		s_freq             : Sampling frequency
-		block_s            : Size of the block in seconds (default = 60)
-	"""
-
-	# Number of clips
-	n_clips = len(clips)
-
-	# For test only
-	if target is None:
-		test_dict = {}
-
-	# Initiate
-	block_len = s_freq * block_s   # Length of each block 
-	X_full = np.zeros((n_clips*n_blocks, block_len, n_ch))
-	Y_full = np.zeros((n_clips*n_blocks,1))
-
-	# Loop over all clips and store data
-	iclip = 0
-	print("Target: ", target if target is not None else "test")
-	for fil in clips:
-		clip = loadmat(fil)
-		segment_name = [el for el in list(clip.keys()) if "segment" in el][0] # Get segment name
-		input_segment = clip[segment_name][0][0][0] # Get electrode data
-		sampling_freq = np.squeeze(clip[segment_name][0][0][1]) # Sampling frequency
-		assert sampling_freq == s_freq, "Wrong sampling frequency!"
-
-		# Get number of channels
-		n_channels = clip[segment_name][0][0][0].shape[0]
-		assert n_channels == n_ch, "Wrong number of channels!"
-
-		# Get tensor input and targets from blocks
-		X, Y = cnn_tensor(input_segment, target, n_channels, n_blocks, block_len)
-
-		X_full[iclip*n_blocks:(iclip+1)*n_blocks,:,:] = X
-		if target is not None:
-			Y_full[iclip*n_blocks:(iclip+1)*n_blocks] = Y[:,None]
-		else:
-			Y = None
-
-			# Test set
-			clip_name = os.path.split(fil)[-1]
-			start = iclip * n_blocks
-			stop = start + n_blocks
-			test_dict[clip_name] = np.arange(start,stop, dtype=int).tolist()
-
-		iclip +=1
-		print("Processed {:d} of {:d} clips".format(iclip, len(clips)))
-
-
-	# Return
-	if target is not None:
-		return X_full, Y_full
-	else:
-		return X_full, test_dict
-
-## Collect all the data and print tensors for CNN
-def read_data_create_tensors(experiment_name, n_blocks = 6, reshuffle = False, save = False):
-	""" Read data from all clips and create tensors
-	"""
-	# Path
-	path_name = "../data/" + experiment_name
-
-	# Read data
-	clips_interictal, clips_preictal, clips_test = get_data(data_folder = path_name)
-
-	# Collect all tensors
-	X_0, Y_0 = cnn_build_input(clips_interictal, 0)
-	X_1, Y_1 = cnn_build_input(clips_preictal, 1)
-	#X_test, test_dict = cnn_build_input(clips_preictal, None)
-
-	# Combine interictal and preictal
-	X = np.concatenate((X_0, X_1), axis=0)
-	Y = np.squeeze(np.concatenate((Y_0, Y_1), axis=0)).astype(int)
-
-	# Save id numbers for each clip by block size
-	clip_ids = [n_blocks*[i] for i in range(len(clips_interictal+clips_preictal))]
-	clip_ids = np.array(clip_ids, dtype=int).reshape(-1)
-
-	# Reshuffle
-	if (reshuffle):
-		np.random.seed(1)
-		shuffle = np.random.choice(np.arange(len(Y)), size=len(Y), replace=False)
-		X = X[shuffle]
-		Y = Y[shuffle]
-		clips_ids = clip_ids[shuffle]
-
-	# Save for later
-	if (save):
-		save_path = "../data/cnn/"
-		fil_X = save_path + experiment_name + "_X.npy"
-		fil_Y = save_path + experiment_name + "_Y.npy"
-		fil_clip = save_path + experiment_name + '_clip_ids.npy'
-		#fil_test = save_path + experiment_name + "_test.npy"
-		np.save(file = fil_X, arr = X)
-		np.save(file = fil_Y, arr = Y)
-		np.save(file = fil_clip, arr = clip_ids)
-		#np.save(file = fil_test, arr = X_test)
-
-		# Dictionary for test set
-		#with open(save_path + experiment_name +'_test_dict.pickle', 'wb') as handle:
-		#	pickle.dump(test_dict, handle)
-	else:
-		return X, Y, clip_ids #, X_test
-
-def concat_Dogs(block_s, save = True):
-	""" Concatenate data from all dogs """
-
-	# List of tensors
-	tensor_list_X = []
-	tensor_list_Y = []
-	clip_id_dict = {}
-	#tensor_list_Xtest = []
-
-	dogs = ['Dog_1', 'Dog_2', 'Dog_3', 'Dog_4']
-	for dog in dogs:
-		X, Y, clip_ids, X_test = read_data_create_tensors(dog)
-		tensor_list_X.append(X)
-		tensor_list_Y.append(Y)
-		#tensor_list_Xtest.append(X_test)
-		clip_id_dict[dog] = clip_id_dict
-
-	# Concatenate the data
-	X = np.concatenate(tensor_list_X)
-	Y = np.concatenate(tensor_list_Y)
-	#X_test = np.concatenate(tensor_list_Xtest)
-
-	# Save
-	if (save):
-		save_path = "../data/cnn/"
-		prefix = str(window) + "_" + str(stride) + "_" + str(block_s)
-		fil_X = save_path + prefix + "_X.npy"
-		fil_Y = save_path + prefix + "_Y.npy"
-		#fil_test = save_path + prefix + "_test.npy"
-		np.save(file = fil_X, arr = X)
-		np.save(file = fil_Y, arr = Y)
-		#np.save(file = fil_test, arr = X_test)
-
-		# Dictionary for test set
-		#with open(save_path + prefix +'_clipId_dict.pickle', 'wb') as handle:
-		#	pickle.dump(clip_id_dict, handle)
-	else:
-		return X, Y, clip_id_dict # X_test
